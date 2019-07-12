7564972a28c1501e47e6b52c669fb47f43403e58
mguilli <matthew.guilliams@gmail.com>
2019-07-12 14:01:46 -0500

    Finished Lesson 2 calculator refactoring

--- RB101/work_temp_lesson_2/calculator_refactor.rb
+++ RB101/work_temp_lesson_2/calculator_refactor.rb
@@ -2,32 +2,69 @@ def prompt(message)
   puts "=> #{message}"
 end
 
-prompt('Welcome to Calculator!')
-
-prompt('What is the first number?')
-num1 = gets.chomp.to_i
-
-prompt('What is the second number?')
-num2 = gets.chomp.to_i
-
-prompt('What operation would you like to perform?')
-prompt('1 - Add')
-prompt('2 - Subtract')
-prompt('3 - Multiply')
-prompt('4 - Divide')
-operator = gets.chomp.to_i
-
-result = case operator
-         when 1
-           num1 + num2
-         when 2
-           num1 - num2
-         when 3
-           num1 * num2
-         when 4
-           num2 / num2.to_f
-         else
-           return(prompt('No valid operator specified!'))
+def valid_number?(num)
+  num.to_f != 0
 end
 
-prompt("Your result is: #{result}")
+def get_number(str)
+  num = nil
+
+  loop do
+    prompt("What is the #{str} number?")
+    num = gets.chomp
+
+    break(num.to_f) if valid_number?(num)
+
+    prompt('Error. Please enter a valid number.')
+  end
+end
+
+def operator_to_message(operator)
+  %w[Adding Subtracting Multiplying Dividing][operator - 1]
+end
+
+operator_prompt = <<~MSG
+  What operation would you like to perform?
+    1 - Add
+    2 - Subtract
+    3 - Multiply
+    4 - Divide
+MSG
+
+prompt('Welcome to Calculator!')
+
+loop do
+  num1 = get_number('first')
+  num2 = get_number('second')
+
+  prompt(operator_prompt)
+
+  operator = 0
+  loop do
+    operator = gets.chomp.to_i
+
+    break if (1..4).include?(operator)
+
+    prompt('Please use a valid operation selection (1,2,3, or 4).')
+  end
+
+  prompt("#{operator_to_message(operator)} the two numbers...")
+
+  result = case operator
+           when 1
+             num1 + num2
+           when 2
+             num1 - num2
+           when 3
+             num1 * num2
+           when 4
+             num1 / num2
+           end
+
+  prompt("Your result is: #{result}")
+
+  prompt('Would you like to perform another calculation (y or n)?')
+  decision = gets.chomp.downcase
+
+  break(prompt('Program exited')) unless %w[y yes].include?(decision)
+end
