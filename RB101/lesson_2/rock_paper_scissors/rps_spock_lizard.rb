045e05616c69c7db478bb30939f01f4582361d67
mguilli <matthew.guilliams@gmail.com>
2019-07-15 15:39:14 -0500

    Modified RPS-spock-lizard to track number of wins

--- RB101/lesson_2/rock_paper_scissors/rps_spock_lizard.rb
+++ RB101/lesson_2/rock_paper_scissors/rps_spock_lizard.rb
@@ -1,6 +1,7 @@
 # Rock Paper Scissors game
 
 VALID_CHOICES = %w[Rock Paper Scissors SPock Lizard].freeze
+GRAND_WINNER_LIMIT = 5
 
 def prompt(message = nil)
   str = "=> #{message}"
@@ -18,14 +19,21 @@ def display_results(player, computer)
   prompt(player_wins?(player, computer) ? 'You won!' : 'Computer won. :(')
 end
 
+def update_win_counts(player, computer, counter)
+  return if player == computer
+
+  player_wins?(player, computer) ? counter[0] += 1 : counter[1] += 1
+end
+
+win_counts = [0, 0]
 loop do
   choice = ''
   loop do
     prompt("Enter all or part of name of a selection: #{VALID_CHOICES}")
     prompt('Selection is not Case Sensitve. Enter "sp" for spock)')
-    prompt()
+    prompt
     choice = gets.chomp
-    choice = VALID_CHOICES.find { |c| /^#{choice}/i === c }
+    choice = VALID_CHOICES.find { |c| /^#{choice}/i =~ c }
 
     break if VALID_CHOICES.include?(choice)
 
@@ -36,12 +44,18 @@ loop do
 
   prompt("You chose: #{choice} | Computer chose: #{computer_choice}")
 
+  update_win_counts(choice, computer_choice, win_counts)
   display_results(choice, computer_choice)
+  prompt("Player, Computer wins: #{win_counts}")
+  break unless win_counts.max < GRAND_WINNER_LIMIT
 
   prompt('Do you want to play again?')
-  prompt()
+  prompt
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
 
+index = win_counts.index(GRAND_WINNER_LIMIT) || 2
+grand_winner = %w[Player Computer No\ one][index]
+prompt("#{grand_winner} is the Grand Winner!!!")
 prompt('Thank you for playing. Good bye!')
