3e86bb6daa46e3c4068cd71b79cb50f9d676e521
mguilli <matthew.guilliams@gmail.com>
2019-10-15 15:51:36 -0500

    Interim

--- RB120/rps-oop/rps.rb
+++ RB120/rps-oop/rps.rb
@@ -1,43 +1,111 @@
 class Player
-  def initialize
-    
+  attr_accessor :move, :name
+
+  def initialize(player_type = :human)
+    @player_type = player_type
+    @move = nil
+    set_name
+  end
+
+  def set_name
+    self.name = if human?
+                  n = ''
+                  loop do
+                    puts 'What is your name?'
+                    n = gets.chomp
+                    break unless n.empty?
+                    puts 'Please enter a valid name.'
+                  end
+
+                  n
+                else
+                  %w(RsDs Hal Chappie Sonny Number\ 5).sample
+                end
   end
 
   def choose
-  end  
-end
+    if human?
+      choice = nil
 
-class Move
-  def initialize
-    
+      loop do
+        puts 'Please choose rock, paper, or scissors:'
+        choice = gets.chomp
+        break if %w(rock paper scissors).include? choice
+        puts 'Invalid choice.'
+      end
+
+      self.move = choice
+    else
+      self.move = %w(rock paper scissors).sample
+    end
+  end
+
+  def human?
+    @player_type == :human
   end
 end
 
-class Rule
-  def initialize
-    
-  end
-end
-
-def compare(move1, move2)
-  
-end
-
+# Game orchestration engine
 class RPSGame
   attr_accessor :human, :computer
 
   def initialize
     @human = Player.new
-    @computer = Player.new
+    @computer = Player.new(:computer)
+  end
+
+  def display_welcome_message
+    puts 'Welcome to Rock, Paper, Scissors!'
+  end
+
+  def display_goodbye_message
+    puts 'Thanks for playing RPS!'
+  end
+
+  def display_winner
+    puts "You chose #{human.move}"
+    puts "The computer chose #{computer.move}"
+
+    case human.move
+    when 'rock'
+      puts "It's a tie!" if computer.move == 'rock'
+      puts 'You won!' if computer.move == 'scissors'
+      puts 'Computer won!' if computer.move == 'paper'
+    when 'paper'
+      puts "It's a tie!" if computer.move == 'paper'
+      puts 'You won!' if computer.move == 'rock'
+      puts 'Computer won!' if computer.move == 'scissors'
+    when 'scissors'
+      puts "It's a tie!" if computer.move == 'scissors'
+      puts 'You won!' if computer.move == 'paper'
+      puts 'Computer won!' if computer.move == 'rock'
+    end
+  end
+
+  def play_again?
+    answer = nil
+
+    loop do
+      puts 'Would you like to play again? (y/n)?'
+      answer = gets.chomp.downcase
+      break if ['y', 'n'].include? answer
+      puts 'Invalid choice.'
+    end
+
+    answer == 'y'
   end
 
   def play
     display_welcome_message
-    human.choose
-    computer.choose
-    display_winner
+
+    loop do
+      human.choose
+      computer.choose
+      display_winner
+      break unless play_again?
+    end
     display_goodbye_message
   end
 end
 
-# RPSGame.new.play
+RPSGame.new.play
