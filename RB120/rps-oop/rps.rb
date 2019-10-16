3b2b53b5a3c7c5ec0003acc9e2fed9a8f413f0ed
mguilli <matthew.guilliams@gmail.com>
2019-10-15 19:25:23 -0500

    Finished basic OOP RPS game

--- RB120/rps-oop/rps.rb
+++ RB120/rps-oop/rps.rb
@@ -1,47 +1,78 @@
+class Move
+  attr_reader :value
+  VALUES = %w(rock paper scissors)
+
+  def initialize(value)
+    @value = value
+  end
+
+  def scissors?
+    @value == 'scissors'
+  end
+
+  def rock?
+    @value == 'rock'
+  end
+
+  def paper?
+    @value == 'paper'
+  end
+
+  def >(other_move)
+    rock? && other_move.scissors? ||
+      paper? && other_move.rock? ||
+      scissors? && other_move.paper?
+  end
+
+  def <(other_move)
+    rock? && other_move.paper? ||
+      paper? && other_move.scissors? ||
+      scissors? && other_move.rock?
+  end
+end
+
 class Player
   attr_accessor :move, :name
 
-  def initialize(player_type = :human)
-    @player_type = player_type
-    @move = nil
+  def initialize
     set_name
   end
+end
 
+class Human < Player
   def set_name
-    self.name = if human?
-                  n = ''
-                  loop do
-                    puts 'What is your name?'
-                    n = gets.chomp
-                    break unless n.empty?
-                    puts 'Please enter a valid name.'
-                  end
+    n = ''
+    loop do
+      puts 'What is your name?'
+      n = gets.chomp
+      break unless n.empty?
+      puts 'Please enter a valid name.'
+    end
 
-                  n
-                else
-                  %w(RsDs Hal Chappie Sonny Number\ 5).sample
-                end
+    self.name = n
   end
 
   def choose
-    if human?
-      choice = nil
+    choice = nil
 
-      loop do
-        puts 'Please choose rock, paper, or scissors:'
-        choice = gets.chomp
-        break if %w(rock paper scissors).include? choice
-        puts 'Invalid choice.'
-      end
-
-      self.move = choice
-    else
-      self.move = %w(rock paper scissors).sample
+    loop do
+      puts 'Please choose rock, paper, or scissors:'
+      choice = gets.chomp
+      break if Move::VALUES.include? choice
+      puts 'Invalid choice.'
     end
+
+    self.move = Move.new choice
+  end
+end
+
+class Computer < Player
+  def set_name
+    self.name = %w(RsDs Hal Chappie Sonny Number\ 5).sample
   end
 
-  def human?
-    @player_type == :human
+  def choose
+    self.move = Move.new(Move::VALUES.sample)
   end
 end
 
@@ -50,8 +81,8 @@ class RPSGame
   attr_accessor :human, :computer
 
   def initialize
-    @human = Player.new
-    @computer = Player.new(:computer)
+    @human = Human.new
+    @computer = Computer.new
   end
 
   def display_welcome_message
@@ -62,23 +93,18 @@ class RPSGame
     puts 'Thanks for playing RPS!'
   end
 
-  def display_winner
-    puts "You chose #{human.move}"
-    puts "The computer chose #{computer.move}"
+  def display_moves
+    puts "#{human.name} chose #{human.move.value}"
+    puts "#{computer.name} chose #{computer.move.value}"
+  end
 
-    case human.move
-    when 'rock'
-      puts "It's a tie!" if computer.move == 'rock'
-      puts 'You won!' if computer.move == 'scissors'
-      puts 'Computer won!' if computer.move == 'paper'
-    when 'paper'
-      puts "It's a tie!" if computer.move == 'paper'
-      puts 'You won!' if computer.move == 'rock'
-      puts 'Computer won!' if computer.move == 'scissors'
-    when 'scissors'
-      puts "It's a tie!" if computer.move == 'scissors'
-      puts 'You won!' if computer.move == 'paper'
-      puts 'Computer won!' if computer.move == 'rock'
+  def display_winner
+    if human.move > computer.move
+      puts "#{human.name} won!"
+    elsif human.move < computer.move
+      puts "#{computer.name} won!"
+    else
+      puts "It's a tie!"
     end
   end
 
@@ -101,6 +127,7 @@ class RPSGame
     loop do
       human.choose
       computer.choose
+      display_moves
       display_winner
       break unless play_again?
     end
