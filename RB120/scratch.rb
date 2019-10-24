19ef90004cbb215f187eb380a81d4e92d88d0352
mguilli <matthew.guilliams@gmail.com>
2019-10-23 19:51:20 -0500

    Finished RB120 - Lesson 4

--- RB120/scratch.rb
+++ RB120/scratch.rb
@@ -1,1 +1,22 @@
-arr = [1,2,3]
+module Specialty
+  # def initialize(value)
+  #   @module = value
+  # end
+end
+
+class SuperClass
+  def initialize(value)
+    @superclass = value
+  end
+end
+
+class SubClass < SuperClass
+  include Specialty
+  def initialize(value, other_value)
+    @subclass = other_value
+    super(value)
+  end
+end
+
+p SubClass.ancestors
+p SubClass.new(10, 42)
